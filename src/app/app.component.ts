898f4b7285853a76aeabade50c4465cd10c3023b
Jamoliddin <j.m.raxmonberdiyev@gmail.com>
2023-09-22 17:56:51 +0500

    Creating a products interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from '@angular/common/http'
+import { IProduct } from './models/product';
+import { IPagination } from './models/pagination';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,11 +9,12 @@ import {HttpClient} from '@angular/common/http'
 })
 export class AppComponent implements OnInit{
   title = 'TexnoStore';
- products: any[];
+ products: IProduct[];
   constructor(private http: HttpClient){}
  
   ngOnInit(): void {
-    this.http.get('https://localhost:7020/api/Products?pageSize=50').subscribe((response : any)=>{
+    this.http.get('https://localhost:7020/api/Products?pageSize=50').subscribe(
+      (response : IPagination)=>{
       this.products = response.data;
       console.log(response);
     }, error=>{
